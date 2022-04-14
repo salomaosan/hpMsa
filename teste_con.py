20864f8211fa2275c91568cda6adfea02206c3f8
salomao <salomaosan@gmail.com>
2022-04-14 09:55:31 -0300

    Código para realizar conexão com storage

---
+++ teste_con.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8
+
+import os, sys
+import urllib2, ssl
+from hashlib import md5
+from xml.etree.ElementTree import fromstring, ElementTree as ET, dump as ETdump
+import json
+
+PROG = os.path.basename(sys.argv[0]).rstrip('.py')
+PROG_DESC = 'hp-msa client'
+USAGE = """Usage: hp-msa.py <HOSTNAME> <USERNAME> <PASSWORD> [lld|stats|data]"""
+
+class msa_storage(object):
+    username = None
+    password = None
+    hostname = None
+    sessionKey = None
+    zbxData = {'data': []}
+
+    def __init__(self, hostname, username, password):
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+    
+    def _request_url(self, api):
+        return 'https://%s/api/%s' % (self.hostname, api)
+    
+    def _request(self, api):
+        # print(self._request_url(api))
+        if self.sessionKey:
+            req = urllib2.Request(url=self._request_url(api))
+            req.add_header('dataType', 'api-brief')
+            req.add_header('sessionKey', self.sessionKey)
+            xml=urllib2.urlopen(req, context=ssl._create_unverified_context()).read()
+            if len(xml)>10:
+                return ET(fromstring(xml)).getroot()
+        return None
+    
+    def _request_show(self, api):
+        return self._request('/show/' + api)
+     
+    def login(self):
+        return_code = 0
+        req = urllib2.Request(url=self._login_url())
+        req.add_header('dataType', 'api-brief')
+        xml = urllib2.urlopen(req, context=ssl._create_unverified_context()).read()
+        tree = ET(fromstring(xml))
+        for obj in tree.getroot():
+            for prop in obj:
+                if prop.get('name') == 'response-type':
+                    response_type = prop.text
+                if prop.get('name') == 'return-code':
+                    return_code = int(prop.text)
+                if prop.get('name') == 'response':
+                    self.sessionKey = prop.text
+        print(response_type, return_code, self.sessionKey)
+        return return_code
+    
+    def _login_url(self):
+        login = md5( '%s_%s' % (self.username, self.password)).hexdigest()
+        url = 'https://%s/api/login/%s' % ( self.hostname, login)
+        return url
+
+    def logout(self):
+        self._request('exit')
+
+if __name__ == "__main__":
+
+    msa = msa_storage(sys.argv[1], sys.argv[2], sys.argv[3])
+    msa.login()
+    msa.logout()
